30db6b6ea867bfad9f494e8cde807dad799a6a08
Isaac Denning <117934828+idenning2003@users.noreply.github.com>
2025-09-25 12:13:16 -0500

    Add back calc test for later tasks to use

---
+++ src/calc/calc.h
@@ -0,0 +1,14 @@
+#ifndef CALC_CALC_H_
+#define CALC_CALC_H_
+
+/**
+ * @brief Add two numbers together.
+ * @note Function is purely for testing and can be removed.
+ *
+ * @param a
+ * @param b
+ * @return int the total
+ */
+extern int add(int a, int b);
+
+#endif  // CALC_CALC_H_
