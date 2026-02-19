a4f8c6f52ee746a199a640fca97d77da7de551ba
cameron200316 <124210953+cameron200316@users.noreply.github.com>
2026-02-19 14:22:14 -0600

    Working GPG Signature Extraction #24 (#33)
    * Working signature extraction
    
    * Fixing Lint errors with include directories
    
    * Fixed header issue for Lint check
    
    * Update src/examples/signature_extract.c
    
    Co-authored-by: Isaac Denning <117934828+idenning2003@users.noreply.github.com>
    
    * Update src/examples/signature_extract.c
    
    Co-authored-by: Isaac Denning <117934828+idenning2003@users.noreply.github.com>
    
    * Update src/examples/signature_extract.c
    
    Co-authored-by: Isaac Denning <117934828+idenning2003@users.noreply.github.com>
    
    * Update src/examples/signature_test.c
    
    Co-authored-by: Isaac Denning <117934828+idenning2003@users.noreply.github.com>
    
    * Update src/examples/signature_extract.c
    
    Co-authored-by: Isaac Denning <117934828+idenning2003@users.noreply.github.com>
    
    * Update src/examples/signature_extract.c
    
    Co-authored-by: Isaac Denning <117934828+idenning2003@users.noreply.github.com>
    
    * Update src/examples/signature_extract.c
    
    Co-authored-by: Isaac Denning <117934828+idenning2003@users.noreply.github.com>
    
    * Update src/examples/signature_test.c
    
    Co-authored-by: Isaac Denning <117934828+idenning2003@users.noreply.github.com>
    
    * Update src/examples/signature_test.c
    
    Co-authored-by: Isaac Denning <117934828+idenning2003@users.noreply.github.com>
    
    * I hate Lint. I want to push bad code to main
    
    ---------
    
    Co-authored-by: Isaac Denning <117934828+idenning2003@users.noreply.github.com>

---
+++ src/examples/signature_extract.h
@@ -0,0 +1,8 @@
+#ifndef EXAMPLES_SIGNATURE_EXTRACT_H_
+#define EXAMPLES_SIGNATURE_EXTRACT_H_
+
+extern char* signature_extract(const char* repo_path, const char* commit_sha);
+extern char* get_head_commit_sha(git_repository* repo);
+extern char* find_git_repo_path(void);
+
+#endif  // EXAMPLES_SIGNATURE_EXTRACT_H_
